edfb167f490cc012f23cea876fd118e0c66d0ae0
Thales Aguiar <thalesaguiar21@gmail.com>
2019-05-30 09:39:27 -0300

    Converted tabs to spaces

--- ageofviking/item.py
+++ ageofviking/item.py
@@ -1,27 +1,27 @@
 
 class Item(object):
-	
-	''' Classe que define um item no jogo '''
+    
+    ''' Classe que define um item no jogo '''
 
-	def __init__(self, name, description, collectable = True, effect = None, weight = 0.0):
-		self.collectable = collectable
-		self.name = name
-		self.description = description
-		self.weight = weight
-		self.effect = effect
+    def __init__(self, name, description, collectable = True, effect = None, weight = 0.0):
+        self.collectable = collectable
+        self.name = name
+        self.description = description
+        self.weight = weight
+        self.effect = effect
 
-	def isCollectable(self):
-		return self.collectable
+    def is_collectable(self):
+        return self.collectable
 
-	def getName(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def getDescription(self):
-		return self.description
+    def getdescription(self):
+        return self.description
 
-	def getWeight(self):
-		return self.weight
+    def getWeight(self):
+        return self.weight
 
-	def getFullDescription(self):
-		return self.name + ', ' + self.description
+    def getFullDescription(self):
+        return self.name + ', ' + self.description
 
